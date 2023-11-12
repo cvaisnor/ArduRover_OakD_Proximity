c97af43e5b32d43410e9ce214a143016d60adc47
Chris Vaisnor <chrisvaisnor@gmail.com>
2023-11-12 10:43:24 -0800

    testing libraries

---
+++ main-dronekit.py
@@ -0,0 +1,17 @@
+# Import DroneKit-Python
+from dronekit import connect, VehicleMode
+
+# Connect to the Vehicle.
+vehicle = connect("/dev/ttyTHS1", baud=57600, wait_ready=True)
+
+# Get some vehicle attributes (state)
+print("Get some vehicle attribute values:")
+print(" GPS: %s" % vehicle.gps_0)
+print(" Battery: %s" % vehicle.battery)
+print(" Last Heartbeat: %s" % vehicle.last_heartbeat)
+print(" Is Armable?: %s" % vehicle.is_armable)
+print(" System status: %s" % vehicle.system_status.state)
+print(" Mode: %s" % vehicle.mode.name)
+
+# Close vehicle object before exiting script
+vehicle.close()
